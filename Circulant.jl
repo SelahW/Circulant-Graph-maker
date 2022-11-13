5eb9750f24dcb14c34c8ba8c2edc3c81673c72b6
SelahW <theokoss2002@gmail.com>
2022-11-13 15:13:39 -0600

    Formatting

--- Circulant.jl
+++ Circulant.jl
@@ -10,18 +10,24 @@ function Circ(n::Integer, j...)
         firstrow[n-j[x]+1] = 1
     end
     A = [firstrow;]
-    d = count(i -> (i >= 1),A)
+    d = count(i -> (i >= 1), A)
     while y < n
-        append!(A,circshift(firstrow,y))
+        append!(A, circshift(firstrow, y))
         y += 1
     end
-    A = reshape(A,(n,n))
-    Laplacian = -A + (d * Matrix(I,n,n))
-    Laplacian = Laplacian[1:end .!= 2, 1:end .!= 2]
+    A = reshape(A, (n, n))
+    Laplacian = -A + (d * Matrix(I, n, n))
+    Laplacian = Laplacian[1:end.!=2, 1:end.!=2]
     G = Graph(A)
-    println("Number of spanning trees: ",trunc(det(Laplacian)))
+    println("Number of spanning trees: ", trunc(det(Laplacian)))
     nodesize = [Graphs.outdegree(G, v) for v in Graphs.vertices(G)]
     nodelabel = 1:nv(G)
     nodelabelsize = nodesize
-    gplot(G,layout = circular_layout,nodesize = nodesize, nodelabel = nodelabel, nodelabelsize = nodelabelsize)
+    gplot(
+        G,
+        layout = circular_layout,
+        nodesize = nodesize,
+        nodelabel = nodelabel,
+        nodelabelsize = nodelabelsize,
+    )
 end
